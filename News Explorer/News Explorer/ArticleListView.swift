e93337ec466b09333b8f20a62a2ff0683778f2f0
Konstantin Kosolapov <kkosolapov@gmail.com>
2023-10-30 13:38:20 +0200

    Update ArticleListView.swift

--- News Explorer/News Explorer/ArticleListView.swift
+++ News Explorer/News Explorer/ArticleListView.swift
@@ -1,77 +1,123 @@
 import SwiftUI
+import Network
 
 struct ArticleListView: View {
-    @ObservedObject var networkManager: NetworkManager
-    @State private var selectedSortingParameter: SortingParameter = .title
-    @State private var selectedArticle: Article?
-    @State private var searchQuery: String = ""
-    @State private var startDate = Date()
-    @State private var endDate = Date()
-        
+    
+    @StateObject var networkManager: NetworkManager
+    @State var selectedSortingParameter: SortingParameter = .title
+    @State var selectedArticle: Article?
+    @State var searchQuery: String = ""
+    @State var noInternetConnection: String = "No network connection"
+    
+    @State var startDate = Date()
+    @State var endDate = Date()
+    
+    @StateObject var monitor = NetworkManager()
+    
+    @State private var showNetworkAlert = false
+    
+    @AppStorage("isDarkMode") private var isDarkMode = false
+    
     var body: some View {
-        VStack {
-            
-            TextField("Search", text: $searchQuery)
-                    .padding()
+        
+        if (monitor.status.rawValue == "disconnected") {
+            VStack {
+                Toggle("Dark Mode", isOn: $isDarkMode)
+                    .padding(.horizontal)
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
+                
+                TextField("Search", text: $searchQuery)
                     .background(Color.gray.opacity(0.1))
                     .cornerRadius(10)
                     .padding(.horizontal)
-        
-            DatePicker("Start Date", selection: $startDate)
-                .datePickerStyle(CompactDatePickerStyle())
-                .padding()
-
-            DatePicker("End Date", selection: $endDate)
-                .datePickerStyle(CompactDatePickerStyle())
-                .padding()
-
-            Picker("Sort By", selection: $selectedSortingParameter) {
-                ForEach(SortingParameter.allCases, id: \.self) { parameter in
-                    Text(parameter.rawValue)
-                }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding()
-
-            List(sortedArticles, id: \.title) { article in
-                Button(action: {
-                    selectedArticle = article
-                }) {
-                    if let title = article.title, let description = article.description {
-                        VStack(alignment: .leading) {
-                            Text("**Title**: \(title)")
-                            Text("**Description**: \(description)")
-                        }
-                        .frame(maxWidth: .infinity,
-                               alignment: .leading)
-                        .padding()
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 4)
-                    } else {
-                        Text("No title")
-                    }
-                }
-            }
-            
-            .sheet(item: $selectedArticle) { article in
-                ArticleDetailView(article: article)
-            }
-            
-            .onAppear {
-                            self.networkManager.fetchArticles { result in
-                                switch result {
-                                case .success:
-                                    break
-                                case .failure(let error):
-                                    print("Error fetching articles: \(error)")
-                    }
-                }
+                
+                DatePicker("Start Date", selection: $startDate)
+                    .datePickerStyle(CompactDatePickerStyle())
+                    .padding()
+                
+                DatePicker("End Date", selection: $endDate)
+                    .datePickerStyle(CompactDatePickerStyle())
+                    .padding()
+                
+                TextField("No Internet", text: $noInternetConnection)
+                    .padding()
+                
             }
         }
+        
+        else {
+            
+            VStack {
+                Toggle("Dark Mode", isOn: $isDarkMode)
+                    .padding(.horizontal)
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
+                
+                TextField("Search", text: $searchQuery)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                
+                DatePicker("Start Date", selection: $startDate)
+                    .datePickerStyle(CompactDatePickerStyle())
+                    .padding()
+                
+                DatePicker("End Date", selection: $endDate)
+                    .datePickerStyle(CompactDatePickerStyle())
+                    .padding()
+                
+                Picker("Sort By", selection: $selectedSortingParameter) {
+                    ForEach(SortingParameter.allCases, id: \.self) { parameter in
+                        Text(parameter.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                List(sortedArticles, id: \.title) { article in
+                    Button(action: {
+                        selectedArticle = article
+                    }) {
+                        if let title = article.title, let description = article.description {
+                            VStack(alignment: .leading) {
+                                Text("**Title**: \(title)")
+                                Text("**Description**: \(description)")
+                            }
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                            .padding()
+                            .background(Color.gray.opacity(0.1))
+                            .cornerRadius(10)
+                            .padding(.horizontal, 4)
+                        } else {
+                            Text("No title")
+                        }
+                    }
+                }
+                
+                .sheet(item: $selectedArticle) { article in
+                    ArticleDetailView(article: article)
+                }
+                
+                .onAppear {
+                    self.networkManager.fetchArticles { result in
+                        switch result {
+                        case .success:
+                            break
+                        case .failure(let error):
+                            print("Error fetching articles: \(error)")
+                        }
+                    }
+                    
+                    
+                    
+                }
+                
+            }
+            
+        }
     }
-
-    private var sortedArticles: [Article] {
+    
+    var sortedArticles: [Article] {
         let filteredArticles: [Article]
         if searchQuery.isEmpty {
             filteredArticles = networkManager.articles
@@ -82,7 +128,7 @@ struct ArticleListView: View {
                 return titleMatch || descriptionMatch
             }
         }
-
+        
         let filteredAndSortedArticles: [Article]
         if startDate <= endDate {
             filteredAndSortedArticles = filteredArticles.filter { article in
@@ -98,7 +144,7 @@ struct ArticleListView: View {
         } else {
             filteredAndSortedArticles = []
         }
-
+        
         switch selectedSortingParameter {
         case .title:
             return filteredAndSortedArticles.sorted { $0.title ?? "" < $1.title ?? "" }
@@ -107,49 +153,7 @@ struct ArticleListView: View {
         case .publishedAt:
             return filteredAndSortedArticles.sorted { $0.publishedAt ?? "" < $1.publishedAt ?? "" }
         }
-
-    }
-}
-
-struct ArticleDetailView: View {
-    var article: Article
-    @State private var image: UIImage? = nil
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            if let imageUrl = article.urlToImage, let _ = URL(string: imageUrl) {
-                if let image = image {
-                    Image(uiImage: image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding()
-                } else {
-                    ProgressView()
-                }
-            }
-            Text("**Title**: \(article.title ?? "")")
-            Text("**Description**: \(article.description ?? "")")
-            Text("**Author**: \(article.author ?? "")")
-            Text("**Source**: \(article.source.name ?? "")")
-            Text("**Published At**: \(article.publishedAt ?? "")")
-        }
-        .onAppear {
-            loadImage()
-        }
-    }
-    
-    private func loadImage() {
-        guard let imageUrlString = article.urlToImage, let imageUrl = URL(string: imageUrlString) else {
-            return
-        }
         
-        URLSession.shared.dataTask(with: imageUrl) { data, response, error in
-            if let data = data, let loadedImage = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.image = loadedImage
-                }
-            }
-        }.resume()
     }
 }
 
